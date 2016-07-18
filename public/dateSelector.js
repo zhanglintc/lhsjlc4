10778e3c480ce7925f830d89b4b65cd8bb8ace95
Lane <zhanglintc623@gmail.com>
2016-07-18 23:13:07 +0800

    day => date

--- public/dateSelector.js
+++ public/dateSelector.js
@@ -1,9 +1,9 @@
 // Refer to: http://www.jb51.net/article/31509.htm
 
-function DateSelector(selYear, selMonth, selDay) {
+function DateSelector(selYear, selMonth, selDate) {
     this.selYear = selYear;
     this.selMonth = selMonth;
-    this.selDay = selDay;
+    this.selDate = selDate;
     this.selYear.Group = this;
     this.selMonth.Group = this;
     // 给年份、月份下拉菜单添加处理onchange事件的函数
@@ -64,14 +64,14 @@ DateSelector.DaysInMonth = function(year, month) {
     return date.getDate();
 }
 // 初始化天数
-DateSelector.prototype.InitDaySelect = function() {
+DateSelector.prototype.InitDateSelect = function() {
     // 使用parseInt函数获取当前的年份和月份
     var year = parseInt(this.selYear.value);
     var month = parseInt(this.selMonth.value);
     // 获取当月的天数
     var daysInMonth = DateSelector.DaysInMonth(year, month);
     // 清空原有的选项
-    this.selDay.options.length = 0;
+    this.selDate.options.length = 0;
     // 循环添加OPION元素到天数select对象中
     for (var i = 1; i <= daysInMonth; i++) {
         // 新建一个OPTION对象
@@ -81,32 +81,32 @@ DateSelector.prototype.InitDaySelect = function() {
         // 设置OPTION对象的内容
         op.innerHTML = i;
         // 添加到天数select对象
-        this.selDay.appendChild(op);
+        this.selDate.appendChild(op);
     }
 }
 // 处理年份和月份onchange事件的方法，它获取事件来源对象（即selYear或selMonth）
-// 并调用它的Group对象（即DateSelector实例，请见构造函数）提供的InitDaySelect方法重新初始化天数
+// 并调用它的Group对象（即DateSelector实例，请见构造函数）提供的InitDateSelect方法重新初始化天数
 // 参数e为event对象
 DateSelector.Onchange = function(e) {
     var selector = window.document.all != null ? e.srcElement: e.target;
-    var date = selector.Group.selDay.value;
-    selector.Group.InitDaySelect();
+    var date = selector.Group.selDate.value;
+    selector.Group.InitDateSelect();
 
     var year = selector.Group.selYear.value;
     var month = selector.Group.selMonth.value;
     var daysInMonth = (new Date(year, month, 0)).getDate();
 
     if(date < daysInMonth) {
-        selector.Group.selDay.value = date;
+        selector.Group.selDate.value = date;
     }
     else {
-        selector.Group.selDay.value = daysInMonth;
+        selector.Group.selDate.value = daysInMonth;
     }
 }
 // 根据参数初始化下拉菜单选项
-DateSelector.prototype.InitSelector = function(year, month, day) {
+DateSelector.prototype.InitSelector = function(year, month, date) {
     // 由于外部是可以调用这个方法，因此我们在这里也要将selYear和selMonth的选项清空掉
-    // 另外因为InitDaySelect方法已经有清空天数下拉菜单，因此这里就不用重复工作了
+    // 另外因为InitDateSelect方法已经有清空天数下拉菜单，因此这里就不用重复工作了
     this.selYear.options.length = 0;
     this.selMonth.options.length = 0;
     // 初始化年、月
@@ -116,7 +116,7 @@ DateSelector.prototype.InitSelector = function(year, month, day) {
     this.selYear.selectedIndex = this.MaxYear - year;
     this.selMonth.selectedIndex = month - 1;
     // 初始化天数
-    this.InitDaySelect();
+    this.InitDateSelect();
     // 设置天数初始值
-    this.selDay.selectedIndex = day - 1;
+    this.selDate.selectedIndex = date - 1;
 }
