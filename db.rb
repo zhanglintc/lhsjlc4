6d3252c272404c9020ac3f0d591d5e5624d02036
Lane <zhanglintc623@gmail.com>
2016-07-20 16:30:23 +0800

    db.rb

---
+++ db.rb
@@ -0,0 +1,48 @@
+#!/env/bin/ruby
+# encoding: utf-8
+
+require 'sqlite3'
+
+class DBManager
+  """
+  Table AVAILABLE_INFO:
+  [0]serial         -- char[255]
+  [1]start          -- int
+  [2]end            -- int
+  """
+
+  def initialize
+    @db = SQLite3::Database.new "./lhsjlc4.db"
+    createDB
+  end
+
+  def createDB
+    begin
+      @db.execute "create table AVAILABLE_INFO(serial char[255], start int, end int)"
+    rescue Exception => e
+    end
+  end
+
+  def publish info
+    @db.execute "insert into AVAILABLE_INFO values(?, ?, ?)", info
+  end
+
+  # def updateDB row
+  #   @db.execute "update AVAILABLE_INFO set memTimes = #{row[2]}, remindTime = #{row[3]}, remindTimeStr = '#{row[4]}' where wordID = '#{row[5]}'" if not COMM::REVERSE_MODE
+  # end
+
+  # def deleteDB serial
+  #   found = @db.execute("select * from AVAILABLE_INFO where serial = #{serial}").size != 0 ? true : false
+  #   @db.execute "delete from AVAILABLE_INFO where serial = #{serial}"
+
+  #   return found
+  # end
+
+  def view
+    @db.execute "select * from AVAILABLE_INFO"
+  end
+
+  def closeDB
+    @db.close
+  end
+end
--- lhsjlc4.rb
+++ lhsjlc4.rb
@@ -1,5 +1,10 @@
+#!/env/bin/ruby
+# encoding: utf-8
+
 require 'sinatra'
 require 'time'
+
+require './db.rb'
 
 set :bind, '0.0.0.0'
 
@@ -16,9 +21,15 @@ get '/publish' do
 end
 
 post '/publish' do
-  time_s = Time.at(params[:from].to_i / 1000)
-  time_e = Time.at(params[:to].to_i / 1000)
+  time_s = params[:from].to_i / 1000
+  time_e = params[:to].to_i / 1000
   serial = params[:serial]
+
+  info = [serial, time_s, time_e]
+
+  dbMgr = DBManager.new
+  dbMgr.publish info
+  dbMgr.closeDB
 
   redirect 'view'
 end
