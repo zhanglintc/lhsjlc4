20f9d1f92dbd7257b2e9c8454bd0687c03add800
Lane <zhanglintc623@gmail.com>
2016-07-21 17:23:30 +0800

    radio: area & getRadioValue() by @Pang327

--- public/utils.js
+++ public/utils.js
@@ -29,3 +29,13 @@ var post = function(url, params) {
 
     return form;
 }
+
+function getRadioValue(radio) {
+    var obj = window.document.getElementsByName(radio);
+
+    for(i = 0; i < obj.length; i++) {
+        if(obj[i].checked == true) {
+            return obj[i].value;
+        }
+    }
+}
